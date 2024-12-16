e786665345fb10eaa22934298bb69195ccd404fe
Aaryashretha <shresthaaarya404@gmail.com>
2024-12-16 20:20:48 +0545

    Lab-1

--- Lab-1/listStudent.php
+++ Lab-1/listStudent.php
@@ -34,7 +34,7 @@ $records = getAllStudents();
 </head>
 <body>
     <h1 align="center">List Record</h1>
-    <a href="addstudent.php" class="text-light no-decor ">Add record</a>
+    <a href="addStudent.php" class="text-light no-decor ">Add record</a>
     <?php  echo displayErrorMessage($err,'failed')?>
     <?php  echo displaySuccessMessage($err,'success')?>
     <table width="100%" border="1">
@@ -47,21 +47,25 @@ $records = getAllStudents();
             <th>Phone</th>
             <th>Address</th>
             <th>Date of Birth</th>
+            <th>Created At</th>
+            <th>Updated At</th>
             <th>Actions</th>
         </tr>
         <?php foreach ($records as $key => $record) { ?>
             <tr>
                 <td><?php echo $key+1 ?></td>
-                <td><?php echo $record['name']; ?></td>
-                <td><?php echo $record['course_id']; ?></td>
+                <td><?php echo $record['name'] ?></td>
+                <td><?php echo $record['course_id'] ?></td>
                 <td><?php echo $record['fee']; ?></td>
-                <td><?php echo $record['rollno']; ?></td>
-                <td><?php echo $record['phone']; ?></td>
-                <td><?php echo $record['address']; ?></td>
-                <td><?php echo $record['dob']; ?></td>
+                <td><?php echo $record['rollno'] ?></td>
+                <td><?php echo $record['phone'] ?></td>
+                <td><?php echo $record['address'] ?></td>
+                <td><?php echo $record['dob'] ?></td>
+                <td><?php echo $record['created_at']; ?></td>
+                <td><?php echo $record['updated_at']; ?></td>
                 <td>
-                    <a href="editStudent.php?edtid=<?php echo $record['id'] ?>">Edit</a>
-                    <a href="liststudent.php?delid=<?php echo $record['id'] ?>">Delete</a>
+                    <a href="editStudent.php?edtid=<?php echo $record['id'] ?>" onclick="return confirm('Are you sure to edit?')">Edit</a>
+                    <a href="listStudent.php?delid=<?php echo $record['id'] ?>"  onclick="return confirm('Are you sure to delete?')">Delete</a>
                 </td>
             </tr>
         <?php } ?>
